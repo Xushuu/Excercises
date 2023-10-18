ef42d047affbf083d7fe98a607b2fea378f8f663
Webert Melo <102331634+Xushuu@users.noreply.github.com>
2023-10-18 10:26:32 -0300

    Add files via upload

---
+++ TABUADA.py
@@ -0,0 +1,14 @@
+#Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada
+
+n = int(input('Digite um numero inteiro Qualquer: '))
+n1 = n*1
+n2 = n*2
+n3 = n*3
+n4 = n*4
+n5 = n*5
+n6 = n*6
+n7 = n*7
+n8 = n*8
+n9 = n*9
+n10 = n*10
+print('{0} x 1 = {1}\n{0} x 2 = {2}\n{0} x 3 = {3}\n{0} x 4 = {4}\n{0} x 5 = {5}\n{0} x 6 = {6}\n{0} x 7 = {7}\n{0} x 8 = {8}\n{0} x 9 = {9}\n{0} x 10 = {10} '.format(n, n1, n2,n3, n4, n5, n6, n7, n8, n9, n10))
